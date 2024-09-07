2485c92cefa4aa6b40da85ea4c309b2afa792aff
Irina Alferova <irinaalferova13@mail.ru>
2024-09-07 12:10:26 +0300

    changes were added

--- src/frontend/src/components/Header/HeaderRouter.tsx
+++ src/frontend/src/components/Header/HeaderRouter.tsx
@@ -11,13 +11,13 @@ import UserHeader from "./User";
 export const HeaderRouter: React.FC<{}> = () => {
     return <BrowserRouter>
         <Routes>
-            <Route path="/" element={<Header title="Все рейсы" />} />
+            <Route path="/" element={<Header title="" />} />
             <Route path="/auth/signin" element={<Header title="Вход" />} />
             <Route path="/auth/signup" element={<Header title="Регистрация" undertitle="Зарегистрируйте нового пользователя" />} />
             <Route path="/flights/:fligtNumber" element={<FlightHeader title="" />} />
             <Route path="/tickets" element={<Header title="Личный кабинет" />} />
             <Route path="/statistics" element={<Header title="Статистика сервиса" />} />
-            <Route path="/flights" element={<Header title="Доступные рейсы"/>} />
+            <Route path="/flights" element={<Header title="Доступные рейсы" />} />
             <Route path="*" element={<Header title="Страница не найдена" />} />
         </Routes>
     </BrowserRouter>
